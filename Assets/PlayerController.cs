3555289f7b76e145e07c9659d207e0a537c6d7bf
saltanat288 <hyxgasfbi69@gmail.com>
2026-05-08 03:21:40 +0500

    Code fixing and adding a fire flare.

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,27 +1,50 @@
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.InputSystem;
+
 public class PlayerController : MonoBehaviour
 {
     public float thrustForce = 1f;
-    Rigidbody2D rb;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float maxSpeed = 5f;
+    public GameObject boosterFlame;
+
+    private Rigidbody2D rb;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Mouse.current.leftButton.isPressed)
         {
             Vector3 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.value);
-            Debug.Log("Mouse position; " + mousePos);
-            Debug.Log("Mouse was pressed.");
-            Vector2 direction = mousePos - transform.position;
+            Vector2 direction = (mousePos - transform.position).normalized;
+
             transform.up = direction;
             rb.AddForce(direction * thrustForce);
+
+            if (rb.velocity.magnitude > maxSpeed)
+            {
+                rb.velocity = rb.velocity.normalized * maxSpeed;
+            }
+        }
+
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            boosterFlame.SetActive(true);
+        }
+        else if (Mouse.current.leftButton.wasReleasedThisFrame)
+        {
+            boosterFlame.SetActive(false);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(gameObject);
         }
     }
 }
