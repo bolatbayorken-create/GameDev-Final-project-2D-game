9839566bd6b0bfc2234b9247c501a3386c8114e4
bolatbayorken-create <bolatbayorken@gmail.com>
2026-05-12 01:46:39 +0500

    Tutorial 5 and fixing code

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,8 +1,15 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
+
 
 public class PlayerController : MonoBehaviour
 {
+    private float elapsedTime = 0f;
+
+    private float score = 0f;
+    public float scoreMultiplier = 10f;
+
     public float thrustForce = 1f;
     public float maxSpeed = 5f;
     public GameObject boosterFlame;
@@ -10,14 +17,27 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rb;
     private SpriteRenderer sr;
 
+    public UIDocument uiDocument;
+    private Label scoreText;
+
+    private bool isDead = false;
+
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
+        scoreText = uiDocument.rootVisualElement.Q<Label>("ScoreLabel");
     }
 
     void Update()
     {
+        elapsedTime += Time.deltaTime;
+        score = Mathf.FloorToInt(elapsedTime * scoreMultiplier);
+        scoreText.text = "Score: " + score;
+
+
+
         if (Mouse.current.leftButton.isPressed)
         {
             Vector3 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.value);
@@ -26,9 +46,9 @@ public class PlayerController : MonoBehaviour
             transform.up = direction;
             rb.AddForce(direction * thrustForce);
 
-            if (rb.velocity.magnitude > maxSpeed)
+            if (rb.linearVelocity.magnitude > maxSpeed)
             {
-                rb.velocity = rb.velocity.normalized * maxSpeed;
+                rb.linearVelocity = rb.linearVelocity.normalized * maxSpeed;
             }
         }
 
@@ -44,15 +64,18 @@ public class PlayerController : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Enemy"))
+        if (collision.gameObject.CompareTag("Enemy") && !isDead)
         {
+            isDead = true;
+            rb.simulated = false;
             StartCoroutine(BlinkAndDisappear());
         }
     }
 
     private System.Collections.IEnumerator BlinkAndDisappear()
     {
-        // Blink 6 times before disappearing
+        boosterFlame.SetActive(false);
+        // Blink 6 times before disappearing // я хз почему не работает я пытался пофиксить
         for (int i = 0; i < 6; i++)
         {
             sr.enabled = !sr.enabled;
